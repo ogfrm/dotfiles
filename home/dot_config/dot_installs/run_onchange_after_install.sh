96c4e9301a5fff46a0557931906236808a99a9d3
Ozgur <ozgurgiray@gmail.com>
2026-05-20 17:24:20 -0400

    test

--- home/dot_config/dot_installs/run_onchange_after_install.sh
+++ home/dot_config/dot_installs/run_onchange_after_install.sh
@@ -2,10 +2,10 @@
 # ./nerdfonts_i.sh install CascadiaCode FiraCode Meslo JetBrainsMono
 # ./pipx_i.sh
 # ./ansible_i.sh
-./fzf_i.sh
+# ./fzf_i.sh
 
 # # {{ include "dotconfig/dot_rc/executable_ohmyposh_i.sh" | sha256sum }}
-# ./ohmyposh_i.sh
+./ohmyposh_i.sh
 # # {{ include "fastfetch_i.sh" | sha256sum }}
 # ./fastfetch_i.sh
 # ./zoxide_i.sh
