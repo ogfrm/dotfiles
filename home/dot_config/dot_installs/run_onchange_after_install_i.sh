a1f742491c6b48d12d2133cfed27ac159adfaa2f
Ozgur <ozgurgiray@gmail.com>
2026-05-21 07:25:23 -0400

    test

---
+++ home/dot_config/dot_installs/run_onchange_after_install_i.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+UPDATE_ALL='' && UNINSTALL_ALL='' && SYSTEM_ALL=''
+while getopts ":urs" opt; do
+  [[ $opt == u ]] && UPDATE_ALL=' -u'
+  [[ $opt == r ]] && UNINSTALL_ALL=' -r'
+  [[ $opt == s ]] && SYSTEM_ALL=' -s'
+done
+
+APPDEP_ALL="curl unzip git wget tar"
+for name in $APPDEP_ALL ;do
+  if command -v $name >/dev/null 2>&1; then continue; fi
+  if command -v apt >/dev/null 2>&1; then sudo apt update && sudo apt install -y $APPDEP_ALL;
+  elif command -v yum >/dev/null 2>&1; then sudo yum install -y $APPDEP_ALL;
+  elif command -v pacman >/dev/null 2>&1; then sudo pacman -Sy --noconfirm $APPDEP_ALL;
+  elif command -v dnf >/dev/null 2>&1; then sudo dnf install -y $APPDEP_ALL;
+  fi
+  break
+done
+APPINSTALL_ALL="ohmyposh starship zoxide fzf eza fastfetch fresh ripgrep"
+for name in $APPDEP_ALL ;do
+  [ ! -f "./${name}_i.sh" ] && continue
+  echo ./${name}_i.sh $UPDATE_ALL $UNINSTALL_ALL $SYSTEM_ALL
+# # {{ include "dotconfig/dot_rc/${name}_i.sh.sh" | sha256sum }}
+done
+
+# ./nerdfonts_i.sh install CascadiaCode FiraCode Meslo JetBrainsMono
+# ./pipx_i.sh
+# ./ansible_i.sh
