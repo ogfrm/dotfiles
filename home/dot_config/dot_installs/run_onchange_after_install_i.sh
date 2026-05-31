40a6908fbd25e1214dae54f7a1539321ffd1fc1d
Ozgur <ozgurgiray@gmail.com>
2026-05-30 22:22:20 -0400

    test

--- home/dot_config/dot_installs/run_onchange_after_install_i.sh
+++ home/dot_config/dot_installs/run_onchange_after_install_i.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+echo ddddd
 # czupdate_all2() {
 #  cd ~/.config/.installs
 #  chezmoi cat ~/.config/.installs/install_i.sh | bash -s -- -u  # to re-run this
 # }
 
 # alias czremove_all="chezmoi cat ~/.config/.installs/install_i.sh | bash -s -- -r"  # to re-run this
-[ -f "./install_all_i.sh" ] && "./install_all_i.sh"
+SCRIPT_DIR="$( \cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+[ -f "$SCRIPT_DIR/install_all_i.sh" ] && "$SCRIPT_DIR/install_all_i.sh"
