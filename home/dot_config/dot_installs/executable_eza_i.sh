51d55f23936d6408f91cda19c7d52503099c47c8
Ozgur <ozgurgiray@gmail.com>
2026-05-31 00:37:40 -0400

    test

--- home/dot_config/dot_installs/executable_eza_i.sh
+++ home/dot_config/dot_installs/executable_eza_i.sh
@@ -3,45 +3,44 @@
 set -euo pipefail
 
 RUNCOMMAND="eza"
-UPDATE=false UNINSTALL=false SUDO="" INSTALL_DIR="$HOME/.local"
+UPDATE=false UNINSTALL=false
 while getopts ":urs" opt; do
 	case "$opt" in
-	u) UPDATE=true ;;
-	r) UNINSTALL=true ;;
-	s) INSTALL_DIR="/usr/local"; SUDO="sudo" ;;
+	g) UPDATE=true ;;
+	u) UNINSTALL=true ;;
 	esac
 done
+# [[ $EUID -eq 0 ]] && INSTALL_DIR="/usr/local/bin" || INSTALL_DIR="$HOME/.local/bin"
+
+SCRIPT_DIR="$( \cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+"$SCRIPT_DIR/ins_git_latest_i.sh" "$@" --repo "eza-community/eza" --app eza eza
+
+# if [[ -n "$SUDO" ]] && command -v apt >/dev/null 2>&1; then
+#     sudo apt update
+#     sudo apt install -y gpg
+#     sudo mkdir -p /etc/apt/keyrings
+#     wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+#     echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+#     sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+#     sudo apt update
+#     sudo apt install -y eza
+# else
+  # wget -qO- "https://github.com/eza-community/eza/releases/latest/download/eza_${TARGET}.tar.gz" | tar xz
 
 if $UNINSTALL; then
-  rm -f "$HOME/.local/bin/eza" "$HOME/.local/share/bash-completion/completions/eza" "$HOME/.local/share/zsh/site-functions/_eza"
-  sudo rm -f /usr/share/bash-completion/completions/eza /usr/share/zsh/site-functions/_eza /usr/local/bin/eza
-  command -v apt >/dev/null 2>&1 && sudo apt remove -y eza || true
-  sudo rm -f /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-  echo "$RUNCOMMAND uninstallation completed at $INSTALL_DIR"
-  exit 0
-fi
-command -v "$RUNCOMMAND" >/dev/null 2>&1 && ! $UPDATE && exit 0
-
-if [[ -n "$SUDO" ]] && command -v apt >/dev/null 2>&1; then
-    sudo apt update
-    sudo apt install -y gpg
-    sudo mkdir -p /etc/apt/keyrings
-    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-    sudo apt update
-    sudo apt install -y eza
+  # rm -f "$HOME/.local/bin/eza"
+  rm -f "$HOME/.local/share/bash-completion/completions/eza" "$HOME/.local/share/zsh/site-functions/_eza"
+  if [[ $EUID -eq 0 ]]; then
+    # rm -f "/usr/local/bin/eza"
+    rm -f "/usr/local/share/bash-completion/completions/eza" "/usr/local/share/zsh/site-functions/_eza"
+    # command -v apt >/dev/null 2>&1 && sudo apt remove -y eza || true
+    sudo rm -f /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    sudo rm -f /usr/share/bash-completion/completions/eza /usr/share/zsh/site-functions/_eza /usr/local/bin/eza
+  fi
 else
-  SCRIPT_DIR="$( \cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-  "$SCRIPT_DIR/ins_git_latest_i.sh" "$@" --repo "eza-community/eza" --app eza eza
-  # wget -qO- "https://github.com/eza-community/eza/releases/latest/download/eza_${TARGET}.tar.gz" | tar xz
+  # [[ $EUID -eq 0 ]] && BC="/usr/local/share" || BC="$HOME/.local/share"
+  [[ $EUID -eq 0 ]] && BC="/usr/share" || BC="$HOME/.local/share"
+  mkdir -p "$BC/bash-completion/completions" "$BC/zsh/site-functions"
+  wget -qO "$BC/bash-completion/completions/eza" https://github.com/eza-community/eza/raw/main/completions/bash/eza
+  wget -qO "$BC/zsh/site-functions/_eza" https://github.com/eza-community/eza/raw/main/completions/zsh/_eza
 fi
-
-BC="$INSTALL_DIR/share"
-if [[ -n "$SUDO" ]]; then BC=/usr/share; fi
-
-$SUDO mkdir -p "$BC/bash-completion/completions" "$BC/zsh/site-functions"
-$SUDO wget -qO "$BC/bash-completion/completions/eza" https://github.com/eza-community/eza/raw/main/completions/bash/eza
-$SUDO wget -qO "$BC/zsh/site-functions/_eza" https://github.com/eza-community/eza/raw/main/completions/zsh/_eza
-
-echo "eza installed to $INSTALL_DIR"
