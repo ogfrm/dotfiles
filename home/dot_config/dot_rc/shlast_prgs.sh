51df22b270b971c85d3a6270c6847ddfc3abdf2b
Ozgur <ozgurgiray@gmail.com>
2026-06-18 08:52:37 -0400

    test

--- home/dot_config/dot_rc/shlast_prgs.sh
+++ home/dot_config/dot_rc/shlast_prgs.sh
@@ -24,29 +24,16 @@ fi
 #######################################################
 # FZF integrations  CTRL+R: history  CTRL+T: find file or dir  ALT+C search for a directory and cd
 #######################################################
-
-fzfdir=$HOME/.local/share/fzf  # /usr/local/share/fzf $HOME/.local/share/fzf
-[ -d "$fzfdir" ] || (git clone --depth 1 https://github.com/junegunn/fzf.git $fzfdir && $fzfdir/install --bin)
-
-if [[ ! "$PATH" == *$fzfdir/bin* ]]; then
-   export PATH="${PATH:+${PATH}:}$fzfdir/bin"
-fi
-
-# Set up fzf key bindings and fuzzy completion  # eval "$(fzf --bash)" # source <(fzf --zsh) # fzf --fish | source
-[ -f "$fzfdir/shell/completion.${MYSHELL}" ] && source "$fzfdir/shell/completion.${MYSHELL}"
-[ -f "$fzfdir/shell/key-bindings.${MYSHELL}" ] && source "$fzfdir/shell/key-bindings.${MYSHELL}"
-
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border --preview "cat {}"' # Preview files on right 'cat'
 # export FZF_DEFAULT_OPTS="--color=bg+:24 --border=bold --border=rounded --layout=reverse --margin=3% --color=dark,bg+:24"
 
-# Use ripgrep (rg) if installed for faster, cleaner searches
-if type rg &>/dev/null; then
+if type rg &>/dev/null; then # Use ripgrep (rg) if installed for faster, cleaner searches
   export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
 fi
 
 # Git fzf ^G ( ? this list ^F Files ^B Branches ^T Tags ^R Remotes ^H commit Hashes ^S Stashes ^L reflogs ^W Worktrees ^E Each ref (git for-each-ref)   # https://github.com/junegunn/fzf-git.sh
-[ -d "${fzfdir}-git" ] || (git clone --depth 1 https://github.com/junegunn/fzf-git.sh.git ${fzfdir}-git)
-[ -f "${fzfdir}-git/fzf-git.sh" ] && source "$fzfdir-git/fzf-git.sh"
+[ -f "$HOME/.local/share/fzf-git/fzf-git.sh" ] && source "$HOME/.local/share/fzf-git/fzf-git.sh"
+[ -f "/usrlocal/share/fzf-git/fzf-git.sh" ] && source "/usr/local/share/fzf-git/fzf-git.sh"
 
 ################## Show system information at login
 # if [ -v TMUX ]
